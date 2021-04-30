6c9bed19132f7661254417dfb862c6dc260737a6
ikanago <ikanago@ikanago.dev>
2021-04-30 21:21:45 +0900

    Fix compile error in test

--- tests/static_files.rs
+++ tests/static_files.rs
@@ -6,10 +6,9 @@ use qz::{
     static_files::{StaticDir, StaticFile},
     status::StatusCode,
 };
-use std::io;
 
 #[tokio::test]
-async fn static_file() -> io::Result<()> {
+async fn static_file() -> qz::Result<()> {
     let mut buffer = RequestBuffer::new();
     buffer
         .try_parse(b"GET /index.html HTTP/1.1\r\n\r\n")
@@ -17,7 +16,7 @@ async fn static_file() -> io::Result<()> {
     let request = buffer.complete();
 
     let handler = StaticFile::mount("./tests/index.html")?;
-    let response = handler.call(request).await;
+    let response = handler.call(request).await?;
     assert_eq!(StatusCode::Ok, response.status_code());
     assert_eq!(
         Some(&b"13".to_vec()),
@@ -32,7 +31,7 @@ async fn static_file() -> io::Result<()> {
 }
 
 #[tokio::test]
-async fn static_dir() -> io::Result<()> {
+async fn static_dir() -> qz::Result<()> {
     let mut buffer = RequestBuffer::new();
     buffer
         .try_parse(b"GET /assets/index.html HTTP/1.1\r\n\r\n")
@@ -40,7 +39,7 @@ async fn static_dir() -> io::Result<()> {
     let request = buffer.complete();
 
     let handler = StaticDir::mount("./tests/assets", "/assets");
-    let response = handler.call(request).await;
+    let response = handler.call(request).await?;
     assert_eq!(StatusCode::Ok, response.status_code());
     assert_eq!(
         Some(&b"13".to_vec()),
@@ -55,7 +54,7 @@ async fn static_dir() -> io::Result<()> {
 }
 
 #[tokio::test]
-async fn static_dir_not_found() -> io::Result<()> {
+async fn static_dir_not_found() -> qz::Result<()> {
     let mut buffer = RequestBuffer::new();
     buffer
         .try_parse(b"GET /assets/index.css HTTP/1.1\r\n\r\n")
@@ -63,7 +62,6 @@ async fn static_dir_not_found() -> io::Result<()> {
     let request = buffer.complete();
 
     let handler = StaticDir::mount("./tests/assets", "/assets");
-    let response = handler.call(request).await;
-    assert_eq!(StatusCode::NotFound, response.status_code());
+    assert_eq!(Err(StatusCode::NotFound), handler.call(request).await);
     Ok(())
 }
