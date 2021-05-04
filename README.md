e916442818fa9f270836ec46d0e871de2fcf12f9
ikanago <ikanago@ikanago.dev>
2021-05-04 09:43:16 +0900

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # qz
+
+[![Unit Test](https://github.com/ikanago/qz/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/ikanago/qz/actions/workflows/ci.yml)
+
+qz is HTTP server library for study. The name comes from 給仕, which means waitor in Japanese.
+
+## Build and Test
+Just run following:
+```rust
+cargo test
+```
+
+Run example code:
+```rust
+cargo run --example hello
+```
--- examples/redirect.rs
+++ examples/redirect.rs
@@ -6,7 +6,11 @@ async fn main() -> io::Result<()> {
     ServerBuilder::new(8080)
         .await?
         .route("/hello", Method::Get, |_| async { "Hello" })
-        .route("/obsolete", Method::Get, Redirect::moved_permanently("/hello"))
+        .route(
+            "/obsolete",
+            Method::Get,
+            Redirect::moved_permanently("/hello"),
+        )
         .route("/maintainance", Method::Get, Redirect::found("/hello"))
         .route("/login", Method::Post, Redirect::see_other("/hello"))
         .build()
