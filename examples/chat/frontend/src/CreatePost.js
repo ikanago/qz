58a93b0ed1df49bbc12d88e55f165f144a2a5273
ikanago <ikanago@ikanago.dev>
2021-05-08 10:53:44 +0900

    Component to create a new post

--- examples/chat/frontend/src/CreatePost.js
+++ examples/chat/frontend/src/CreatePost.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+const apiBaseUrl = "http://localhost:8080";
+
 const CreatePost = () => {
     const [name, setName] = useState("");
     const [text, setText] = useState("");
@@ -12,10 +14,32 @@ const CreatePost = () => {
         setText(event.target.value);
     }
 
+    const clearInput = () => {
+        setName("");
+        setText("");
+    }
+
+    const makeRequest = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "json";
+        xhr.open("POST", apiBaseUrl + "/create_post");
+        xhr.send(JSON.stringify({
+            username: name,
+            text: text,
+        }));
+
+        xhr.onload = _e => {
+            if (xhr.status === 200) {
+                clearInput();
+            }
+        };
+    }
+
     return (
         <>
         <input onChange={handleNameInput} value={name} type="text"/>
         <input onChange={handleTextInput} value={text} type="text"/>
+        <button onClick={makeRequest} >Send</button>
         </>
     )
 }
