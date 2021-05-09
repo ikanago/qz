84bf9731cd512e2eeb945f614b3c0e5b7afb8393
ikanago <ikanago@ikanago.dev>
2021-05-09 15:49:40 +0900

    Define header names by macro

--- src/header.rs
+++ src/header.rs
@@ -2,57 +2,56 @@ use std::{convert::From, fmt};
 
 pub type HeaderValue = Vec<u8>;
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-pub enum HeaderName {
-    Accept,
-    Authorization,
-    ContentLength,
-    ContentType,
-    Host,
-    Location,
-    UserAgent,
-    WwwAuthenticate,
-    Unknown,
-}
+macro_rules! define_headers {
+    ($(($name:ident, $upper_str:expr, $lower_str:expr),)+) => {
+        #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+        pub enum HeaderName {
+            $($name,)+
+            Unknown,
+        }
 
-impl AsRef<[u8]> for HeaderName {
-    fn as_ref(&self) -> &[u8] {
-        match self {
-            HeaderName::Accept => b"Accept",
-            HeaderName::Authorization => b"Authorization",
-            HeaderName::ContentLength => b"Content-Length",
-            HeaderName::ContentType => b"Content-Type",
-            HeaderName::Host => b"Host",
-            HeaderName::Location => b"Location",
-            HeaderName::UserAgent => b"User-Agent",
-            HeaderName::WwwAuthenticate => b"WWW-Authenticate",
-            HeaderName::Unknown => b"Unknown",
+        impl AsRef<[u8]> for HeaderName {
+            fn as_ref(&self) -> &[u8] {
+                match self {
+                    $(HeaderName::$name => $upper_str,)+
+                    HeaderName::Unknown => b"Unknown",
+                }
+            }
+        }
+
+        impl From<Vec<u8>> for HeaderName {
+            fn from(mut name: Vec<u8>) -> Self {
+                // RFC2616 says a header name is case insensitive.
+                // https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
+                name.make_ascii_lowercase();
+                match &name[..] {
+                    $($lower_str => HeaderName::$name,)+
+                    _ => HeaderName::Unknown
+                }
+            }
         }
     }
 }
+
+define_headers!(
+    (Accept, b"Accept", b"accept"),
+    (
+        AccessControlAllowOrigin,
+        b"Access-Control-Allow-Origin",
+        b"access-control-allow-origin"
+    ),
+    (Authorization, b"Authorization", b"authorization"),
+    (ContentLength, b"Content-Length", b"content-length"),
+    (ContentType, b"Content-Type", b"content-type"),
+    (Host, b"Host", b"host"),
+    (Location, b"Location", b"location"),
+    (UserAgent, b"User-Agent", b"user-agent"),
+    (WwwAuthenticate, b"WWW-Authenticate", b"www-authenticate"),
+);
 
 impl fmt::Display for HeaderName {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", std::str::from_utf8(self.as_ref()).unwrap())
-    }
-}
-
-impl From<Vec<u8>> for HeaderName {
-    fn from(mut name: Vec<u8>) -> Self {
-        // RFC2616 says header name is case insensitive.
-        // https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
-        name.make_ascii_lowercase();
-        match &name[..] {
-            b"accept" => HeaderName::Accept,
-            b"authorization" => HeaderName::Authorization,
-            b"content-length" => HeaderName::ContentLength,
-            b"content-type" => HeaderName::ContentType,
-            b"location" => HeaderName::Location,
-            b"host" => HeaderName::Host,
-            b"user-agent" => HeaderName::UserAgent,
-            b"www-authenticate" => HeaderName::WwwAuthenticate,
-            _ => HeaderName::Unknown,
-        }
     }
 }
 
